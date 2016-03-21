ccfb7b4bd6ac63f0a124d282ca18332d06913fea
wiproinnovation <sriram.krishnaswamy@wipro.com>
2016-03-21 05:39:38 -0400

    Added files via upload

---
+++ scripts/rsWiproCallingRESTAPI.ps1
@@ -0,0 +1,29 @@
+﻿[Reflection.Assembly]::LoadWithPartialName("System.Net")
+
+# POSTING AN ACTIVITY
+
+$activity_json = '{ "activity":{
+        "actor":{"name":"Govindaraj Rangan","email":"govindaraj.rangan@wipro.com"},
+        "action":"create",
+        "object": {"url":"someURL","title":"LoB Connect"},
+        "message":"Update from Powershell"
+        } }';
+# GET AUTH TOKEN
+# Authorize by going to: https://www.yammer.com/dialog/oauth?client_id=[:client_id]&redirect_uri=[:redirect_uri]
+# Yammer replies with auth_code to: [:redirect_uri]?code=[:auth_code]
+# Use this auth_code to get auth_token.json which has the auth_token using the following URL
+# https://www.yammer.com/oauth2/access_token.json?client_id=[:client_id]&client_secret=[:client_secret]&code=[:auth_code]
+
+$oauth_token = "OAUTH_TOKEN"; #wipro-microsoft
+
+$post_body = [System.Text.Encoding]::ASCII.GetBytes($activity_json); 
+$url = "https://www.yammer.com/api/v1/activity.json";
+[System.Net.HttpWebRequest] $request = [System.Net.WebRequest]::Create($url);
+$request.Method = "POST";
+$request.Headers.Add("Authorization", "Bearer " + $oauth_token);
+$request.ContentType = "application/json";
+$body = $request.GetRequestStream();
+$body.write($post_body, 0, $post_body.length);
+$body.flush();
+$body.close();
+$response = $request.GetResponse();
---
+++ scripts/rsWiproTweetWithOAuth.ps1
@@ -0,0 +1,53 @@
+﻿[Reflection.Assembly]::LoadWithPartialName("System.Security")
+[Reflection.Assembly]::LoadWithPartialName("System.Net")
+
+$json = '{ "activity":{
+        "actor":{"name":"Govindaraj Rangan","email":"govindaraj.rangan@wipro.com"},
+        "action":"create",
+        "object": {"url":"https://someUrl","title":"Lunch Meeting"},
+        "message":"Tweet from Powershell",
+        }
+        }';
+$oauth_consumer_key = "OAUTH_CONSUMER_KEY";
+$oauth_consumer_secret = "OAUTH_CONSUMER_SECRET";
+$oauth_token = "OAUTH_TOKEN";
+$oauth_token_secret = "<auth token secret>";
+$oauth_nonce = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes([System.DateTime]::Now.Ticks.ToString()));
+$ts = [System.DateTime]::UtcNow - [System.DateTime]::ParseExact("01/01/1970", "dd/MM/yyyy", $null).ToUniversalTime();
+$oauth_timestamp = [System.Convert]::ToInt64($ts.TotalSeconds).ToString();
+
+$signature = "POST&";
+$signature += [System.Uri]::EscapeDataString("https://www.yammer.com/api/v1/activity.json") + "&";
+$signature += [System.Uri]::EscapeDataString("oauth_consumer_key=" + $oauth_consumer_key + "&");
+$signature += [System.Uri]::EscapeDataString("oauth_nonce=" + $oauth_nonce + "&"); 
+$signature += [System.Uri]::EscapeDataString("oauth_signature_method=HMAC-SHA1&");
+$signature += [System.Uri]::EscapeDataString("oauth_timestamp=" + $oauth_timestamp + "&");
+$signature += [System.Uri]::EscapeDataString("oauth_token=" + $oauth_token + "&");
+$signature += [System.Uri]::EscapeDataString("oauth_version=1.0&");
+$signature += [System.Uri]::EscapeDataString("status=" + $status);
+
+$signature_key = [System.Uri]::EscapeDataString($oauth_consumer_secret) + "&" + [System.Uri]::EscapeDataString($oauth_token_secret);
+
+$hmacsha1 = new-object System.Security.Cryptography.HMACSHA1;
+$hmacsha1.Key = [System.Text.Encoding]::ASCII.GetBytes($signature_key);
+$oauth_signature = [System.Convert]::ToBase64String($hmacsha1.ComputeHash([System.Text.Encoding]::ASCII.GetBytes($signature)));
+
+$oauth_authorization = 'OAuth ';
+$oauth_authorization += 'oauth_consumer_key="' + [System.Uri]::EscapeDataString($oauth_consumer_key) + '",';
+$oauth_authorization += 'oauth_nonce="' + [System.Uri]::EscapeDataString($oauth_nonce) + '",';
+$oauth_authorization += 'oauth_signature="' + [System.Uri]::EscapeDataString($oauth_signature) + '",';
+$oauth_authorization += 'oauth_signature_method="HMAC-SHA1",'
+$oauth_authorization += 'oauth_timestamp="' + [System.Uri]::EscapeDataString($oauth_timestamp) + '",'
+$oauth_authorization += 'oauth_token="' + [System.Uri]::EscapeDataString($oauth_token) + '",';
+$oauth_authorization += 'oauth_version="1.0"';
+
+$post_body = [System.Text.Encoding]::ASCII.GetBytes("status=" + $status); 
+[System.Net.HttpWebRequest] $request = [System.Net.WebRequest]::Create("https://www.yammer.com/api/v1/activity.json");
+$request.Method = "POST";
+$request.Headers.Add("Authorization", $oauth_authorization);
+$request.ContentType = "application/x-www-form-urlencoded";
+$body = $request.GetRequestStream();
+$body.write($post_body, 0, $post_body.length);
+$body.flush();
+$body.close();
+$response = $request.GetResponse();
