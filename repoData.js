c881261ea30b77b7cdf949ee71cb5e38e5e6f110
Sriram Krishnaswamy <sriram.krishnaswamy@wipro.com>
2016-05-02 11:51:31 +0530

    Added automatic repo

---
+++ repoData.js
@@ -0,0 +1,42 @@
+var repoData = [
+    {
+        name: "Backup-as-a-Service enabled Virtual Machine",
+        description: "Installs VM in VMWare Private Cloud with Commvault agent and Backup policy chosen while ordering",
+        type: "blueprint",
+        viewUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/backupAsAService/baas.xml",
+        designerUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/backupAsAService/baas.zip",
+        launchUrl: "https://124.124.225.172:8444/csa/login",
+        helpUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/backupAsAService/readme.txt",
+        icon: "images/default.png"
+    },
+    {
+        name: "Microsoft Private Cloud POD on nested ESX Hypervisor",
+        description: "A Lab as a service offering to create a Microsoft Private Cloud POD with WAP/SCVMM/SCO and Hyper-V servers",
+        type: "blueprint",
+        viewUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/microsoftPrivateCloud/Microsoft_Private_Cloud_POD.xml",
+        designerUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/microsoftPrivateCloud/Microsoft_Private_Cloud_POD.zip",
+        launchUrl: "https://124.124.225.172:8444/csa/login",
+        helpUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/microsoftPrivateCloud/readme.txt",
+        icon: "images/default.png"
+    },
+    {
+        name: "VMWare vRealize POD on nested ESX Hypervisor",
+        description: "A Lab as a service offering to create a VMWare Private Cloud POD with vRealize suite",
+        type: "blueprint",
+        viewUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/vmwareVRealizePOD/vRealizePOD.xml",
+        designerUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/vmwareVRealizePOD/vRealizePOD.zip",
+        launchUrl: "https://124.124.225.172:8444/csa/login",
+        helpUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/vmwareVRealizePOD/readme.txt",
+        icon: "images/default.png"
+    },
+    {
+        name: "ACI Base Application Network Profile for 3 tier app",
+        description: "ACI base Application Network Profile for 3 tier application, each tier built in separate subnets with security policies",
+        type: "blueprint",
+        viewUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/threeTierApplicationwitNetwork/ACI_Appln_Profile_Reference_Design.xml",
+        designerUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/threeTierApplicationwitNetwork/ACI_Appln_Profile_Reference_Design.zip",
+        launchUrl: "https://124.124.225.172:8444/csa/login",
+        helpUrl: "https://github.com/wiproinnovation/rapidstartrepo/solutionTemplate/hpCSA/threeTierApplicationwitNetwork/readme.txt",
+        icon: "images/default.png"
+    }
+];
